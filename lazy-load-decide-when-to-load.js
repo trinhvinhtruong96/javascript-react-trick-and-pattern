90de65c8201ca0cad79eb81c7a414a6aa54ea9be
trinhvinhtruong96 <48130078+trinhvinhtruong96@users.noreply.github.com>
2023-11-17 20:15:10 +0700

    Lazy load loading time

--- lazy-load-decide-when-to-load.js
+++ lazy-load-decide-when-to-load.js
@@ -1,12 +1,11 @@
 // Code splitting
-// Only load GLobal component when showGlobe = true
-// http://localhost:3000/isolated/final/01.js
+// When mouse enter or focus, load Globe component by using loadGlobe
+// http://localhost:3000/isolated/final/01.extra-1.js
 
 import * as React from 'react'
 
-// With /* webpackPrefetch: true */, webpack tell browser to prefetch dynamic import
-// const Globe = React.lazy(() => import(/* webpackPrefetch: true */ '../globe'))
-const Globe = React.lazy(() => import('../globe'))
+const loadGlobe = () => import('../globe')
+const Globe = React.lazy(loadGlobe)
 
 function App() {
   const [showGlobe, setShowGlobe] = React.useState(false)
@@ -22,7 +21,11 @@ function App() {
         padding: '2rem',
       }}
     >
-      <label style={{marginBottom: '1rem'}}>
+      <label
+        style={{ marginBottom: '1rem' }}
+        onMouseEnter={loadGlobe}
+        onFocus={loadGlobe}
+      >
         <input
           type="checkbox"
           checked={showGlobe}
@@ -30,7 +33,7 @@ function App() {
         />
         {' show globe'}
       </label>
-      <div style={{width: 400, height: 400}}>
+      <div style={{ width: 400, height: 400 }}>
         <React.Suspense fallback={<div>loading globe...</div>}>
           {showGlobe ? <Globe /> : null}
         </React.Suspense>
